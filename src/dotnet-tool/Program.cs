9fff26198e23771f68a33b985fa476423c48bfb3
dagaric <dagaric@DESKTOP-04STLHM>
2016-05-21 18:57:45 +0600

    1

---
+++ src/dotnet-tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNamespace.Tools
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dotnet tool.");
+        }
+    }
+}
